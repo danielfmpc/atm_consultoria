349865e9e979385a84341c38f7b7f0857f1948a5
unknown <daniel_fmpc@hotmail.com.br>
2019-09-03 21:36:34 -0300

    Configurando Tela Principal

---
+++ lib/TelaPrincipal.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TelaPrincipal extends StatefulWidget {
+  @override
+  _TelaPrincipalState createState() => _TelaPrincipalState();
+}
+
+class _TelaPrincipalState extends State<TelaPrincipal> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
